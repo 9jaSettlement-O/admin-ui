77506739fbcfe5db8b5331ba3748563c4bdad70a
temitope9js <temitope.okuselu@9jasettlement.com>
2026-02-16 14:47:44 +0100

    deploy to github pages

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/admin-ui">
         <App />
         <Toaster richColors position="top-right" />
       </BrowserRouter>
