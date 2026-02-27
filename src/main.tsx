394b5343e716d08cf565cb638a872c458fc3b7bf
temitope9js <temitope.okuselu@9jasettlement.com>
2026-02-27 13:44:56 +0100

    refactor: remove basename from HashRouter to reduce PR variability

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryProvider>
-      <Router basename={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <Router>
         <App />
         <Toaster richColors position="top-right" />
       </Router>
